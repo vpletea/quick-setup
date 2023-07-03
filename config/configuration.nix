9139ffac4e98e85dd46ec9fe570e26d9de48b7b3
vpletea <84437690+vpletea@users.noreply.github.com>
2023-07-03 12:07:57 +0300

    Update configuration.nix to use TLP

--- config/configuration.nix
+++ config/configuration.nix
@@ -164,6 +164,7 @@
       gnome.gnome-contacts
       gnome.gnome-maps
       gnome.gnome-music
+      gnome.gnome-power-manager
       gnome.hitori
       gnome.iagno
       gnome.seahorse
@@ -243,6 +244,24 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   networking.firewall.enable = true;
+
+  # TLP
+  services.power-profiles-daemon.enable = false;
+  services.tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+#
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 35;
+      };
+   };
 
 
   nix.gc = {
